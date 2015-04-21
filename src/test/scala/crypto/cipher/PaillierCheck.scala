3983c490940895fdb37b0b9699812bff4284cd98
Markus Hauck <markus1189@gmail.com>
2015-04-21 16:19:52 +0200

    Paillier property for positive integers

--- src/test/scala/crypto/cipher/PaillierCheck.scala
+++ src/test/scala/crypto/cipher/PaillierCheck.scala
@@ -1,16 +1,26 @@
 package crypto.cipher
 
+import org.scalacheck.Gen
 import org.scalacheck.Properties
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Prop.forAll
 
 object PaillierCheck extends Properties("Paillier") {
-  val (encrypt,decrypt,pub) = Paillier.create(512)
+  val positiveInts = arbitrary[BigInt] suchThat (_.signum == 1)
 
-  property("decrypt . encrypt = id") = forAll { (input: BigInt) =>
+  val (encrypt,decrypt,pub) = Paillier.create(1024)
+
+  property("decrypt . encrypt = id for positive ints") =
+    forAll(positiveInts) { (input: BigInt) =>
+      decrypt(encrypt(input)) == input
+    }
+
+  property("decrypt . encrypt = id (with modulus)") = forAll { (input: BigInt) =>
     decrypt(encrypt(input)) == input.mod(pub.n)
   }
 
   property("additive homomorphic") = forAll { (a: BigInt, b: BigInt) =>
     decrypt((encrypt(a) * encrypt(b)) mod pub.nSquare) == (a + b).mod(pub.n)
   }
+
 }
