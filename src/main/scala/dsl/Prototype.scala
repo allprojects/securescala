d0e1cdc859d78bc902dab902c88aa30f5d075f68
Markus Hauck <markus1189@gmail.com>
2015-04-21 15:25:42 +0200

    Comparisons in the DSL

--- src/main/scala/dsl/Prototype.scala
+++ src/main/scala/dsl/Prototype.scala
@@ -1,28 +1,20 @@
 package dsl
 
 import scalaz._
+import Scalaz._
 import scalaz.Free._
+import scalaz.Ordering._
+import scalaz.std.list
 
-sealed trait Enc {
-  def +(that: Enc) = this match {
-    case Unencrypted(n) => that match {
-      case Unencrypted(m) => Unencrypted(n + m)
-    }
-  }
-
-  def *(that: Enc) = this match {
-    case Unencrypted(n) => that match {
-      case Unencrypted(m) => Unencrypted(n * m)
-    }
-  }
-}
+sealed trait Enc
 // case class Paillier(self: BigInt) extends Enc
-case class Unencrypted(self: Int) extends Enc
+case class NoEnc(self: Int) extends Enc // No encryption for testing
 
-sealed trait CryptoF[K]
-case class Mult[K](lhs: Enc, rhs: Enc, K: Enc => K) extends CryptoF[K]
-case class Plus[K](lhs: Enc, rhs: Enc, K: Enc => K) extends CryptoF[K]
-case class Encrypt[K](v: Int, K: Enc => K) extends CryptoF[K]
+sealed trait CryptoF[+K]
+case class Mult[K](lhs: Enc, rhs: Enc, k: Enc => K) extends CryptoF[K]
+case class Plus[K](lhs: Enc, rhs: Enc, k: Enc => K) extends CryptoF[K]
+case class Encrypt[K](v: Int, k: Enc => K) extends CryptoF[K]
+case class Compare[K](lhs: Enc, rhs: Enc, k: Ordering => K)  extends CryptoF[K]
 
 object CryptoF {
   type Crypto[A] = Free[CryptoF, A]
@@ -30,38 +22,73 @@ object CryptoF {
   def multiply(lhs: Enc, rhs: Enc): Crypto[Enc] = liftF(Mult(lhs,rhs,identity))
   def add(lhs: Enc, rhs: Enc): Crypto[Enc] = liftF(Plus(lhs,rhs,identity))
   def encrypt(v: Int): Crypto[Enc] = liftF(Encrypt(v,identity))
+  def compare(lhs: Enc, rhs: Enc): Crypto[Ordering] = liftF(Compare(lhs,rhs,identity))
 
   implicit val functor: Functor[CryptoF] = new Functor[CryptoF] {
     def map[A,B](fa: CryptoF[A])(f: A => B): CryptoF[B] = fa match {
       case Mult(lhs,rhs,k) => Mult(lhs,rhs,f compose k)
       case Plus(lhs,rhs,k) => Plus(lhs,rhs,f compose k)
       case Encrypt(v,k) => Encrypt(v,f compose k)
+      case Compare(lhs,rhs,k) => Compare(lhs,rhs,f compose k)
     }
   }
 
-  def locally(p: Crypto[Enc]): Enc = p.resume match {
-    case -\/(Mult(lhs,rhs,k)) =>
-      println("Multiplication")
-      locally(k(lhs*rhs))
-    case -\/(Plus(lhs,rhs,k)) =>
-      println("Addition")
-      locally(k(lhs+rhs))
+  def interpret(p: Crypto[Enc]): Enc = p.resume match {
+    case -\/(Mult(NoEnc(lhs),NoEnc(rhs),k)) =>
+      println(s"Interpreting: ${lhs} * ${rhs}")
+      interpret(k(NoEnc(lhs*rhs)))
+    case -\/(Plus(NoEnc(lhs),NoEnc(rhs),k)) =>
+      println(s"Interpreting: ${lhs} + ${rhs}")
+      interpret(k(NoEnc(lhs+rhs)))
+    case -\/(Compare(NoEnc(lhs),NoEnc(rhs),k)) =>
+      println(s"Comparing: ${lhs} and ${rhs}")
+      interpret(k(lhs ?|? rhs))
     case -\/(Encrypt(v,k)) =>
-      locally(k(Unencrypted(v)))
-    case \/-(x) => x
+      println(s"Encrypting: ${v}")
+      interpret(k(NoEnc(v)))
+    case \/-(x) =>
+      println(s"Final result: ${x}")
+      x
   }
 }
 
-object Example extends App {
+object ExamplePrograms {
   import CryptoF._
+
+  def encMax(a: Enc, b: Enc) = for {
+    comparison <- compare(a,b)
+    result <- comparison match {
+      case LT => Free.point[CryptoF, Enc](b)
+      case _ => Free.point[CryptoF, Enc](a)
+    }
+  } yield result
+
+  def sum(xs: List[Enc]): Crypto[Enc] = for {
+    init <- encrypt(0)
+    r <- xs.foldLeftM(init) { (accum,x) => add(accum,x) }
+  } yield r
+
+  def product(xs: List[Enc]): Crypto[Enc] = for {
+    init <- encrypt(1)
+    r <- xs.foldLeftM(init) { (accum,x) => multiply(accum,x) }
+  } yield r
 
   def prog1(a: Enc, b: Enc) = for {
     aTimesB <- multiply(a,b)
-    c <- encrypt(42)
+    c <- encrypt(32)
     r <- add(aTimesB, c)
   } yield r
+}
 
-  val myprog = prog1(Unencrypted(5), Unencrypted(2))
-  val Unencrypted(result) = locally(myprog)
-  println("Final result: " + result)
+object Main extends App {
+  import ExamplePrograms._
+  import CryptoF._
+
+  println("Summing a list")
+  def encrypt: Int => Enc = NoEnc(_) // stub
+
+  val encryptedList = List.iterate(1,10)(_ + 1).map(encrypt)
+
+  interpret(sum(encryptedList))
+  interpret(product(encryptedList))
 }
