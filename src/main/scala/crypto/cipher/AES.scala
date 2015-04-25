64b5dac65df6b267e0feff2f39e4f00476881cbd
Markus Hauck <markus1189@gmail.com>
2015-04-25 14:19:59 +0200

    Scala version of AES

---
+++ src/main/scala/crypto/cipher/AES.scala
@@ -0,0 +1,47 @@
+package crypto.cipher
+
+import scalaz.syntax.IdOps
+import scalaz.syntax.id._
+
+import javax.crypto.SecretKey
+import javax.crypto.KeyGenerator
+import javax.crypto.Cipher;
+
+object AES {
+  sealed trait KeySize { def bits: Int }
+  case object B128 extends KeySize { def bits = 128 }
+  case object B192 extends KeySize { def bits = 192 }
+  case object B256 extends KeySize { def bits = 256 }
+
+  private val algorithmString = "AES/ECB/PKCS5Padding"
+
+  private lazy val cipherEncrypt: Cipher =
+    Cipher.getInstance(algorithmString, "SunJCE")
+
+  private lazy val cipherDecrypt: Cipher =
+    Cipher.getInstance(algorithmString, "SunJCE")
+
+  case class Encryptor(f: Array[Byte] => Array[Byte]) extends Function1[Array[Byte],Array[Byte]]{
+    def apply(x: Array[Byte]) = f(x)
+  }
+
+  case class Decryptor(f: Array[Byte] => Array[Byte]) extends Function1[Array[Byte],Array[Byte]]{
+    def apply(x: Array[Byte]) = f(x)
+  }
+
+  type PrivKey = SecretKey
+
+  def create(keySize: KeySize): (Encryptor,Decryptor) = {
+    val key = generateKey(keySize)
+    (Encryptor(encrypt(key)), Decryptor(decrypt(key)))
+  }
+
+  def generateKey(keySize: KeySize): SecretKey =
+    (KeyGenerator.getInstance("AES") <| (_.init(keySize.bits))).generateKey
+
+  private def encrypt(priv: PrivKey)(input: Array[Byte]): Array[Byte] =
+    (cipherEncrypt <| (_.init(Cipher.ENCRYPT_MODE, priv))).doFinal(input)
+
+  private def decrypt(priv: PrivKey)(input: Array[Byte]): Array[Byte] =
+    (cipherDecrypt <| (_.init(Cipher.DECRYPT_MODE, priv))).doFinal(input)
+}
---
+++ src/test/scala/crypto/cipher/AesCheck.scala
@@ -0,0 +1,25 @@
+package crypto.cipher
+
+import org.scalacheck.Gen
+import org.scalacheck.Properties
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.BooleanOperators
+
+object AesCheck extends Properties("AES") {
+  val (encrypt,decrypt) = AES.create(AES.B256)
+
+  property("decrypt · encrypt = id (BigInt)") =
+    forAll { (input: BigInt) =>
+      BigInt(decrypt(encrypt(input.toByteArray))) == input
+    }
+
+  property("deterministic") = forAll { (a: BigInt) =>
+    val c1 = encrypt(a.toByteArray)
+    val c2 = encrypt(a.toByteArray)
+
+    (c1.size == c2.size) :| "same number of bytes" &&
+    ((c1,c2).zipped.forall(_==_)) :| "all bytes equal"
+  }
+
+}
