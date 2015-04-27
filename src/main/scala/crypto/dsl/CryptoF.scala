ea852fe89b455fff39d54cda321e2280bc14b0ba
Markus Hauck <markus1189@gmail.com>
2015-04-27 11:05:52 +0200

    Add subtraction as offline operation to DSL

--- src/main/scala/crypto/dsl/CryptoF.scala
+++ src/main/scala/crypto/dsl/CryptoF.scala
@@ -4,7 +4,7 @@ import crypto.cipher._
 import scalaz._
 
 sealed trait CryptoF[+K]
-case class Mult[K](lhs: Enc, rhs: Enc, k: Enc => K) extends CryptoF[K]
+case class Mult[K](lhs: Enc, rhs: Enc, k: GamalEnc => K) extends CryptoF[K]
 case class Plus[K](lhs: Enc, rhs: Enc, k: PaillierEnc => K) extends CryptoF[K]
 case class Equals[K](lhs: Enc, rhs: Enc, k: Boolean => K)  extends CryptoF[K]
 case class Compare[K](lhs: Enc, rhs: Enc, k: Ordering => K) extends CryptoF[K]
@@ -14,6 +14,9 @@ case class ToPaillier[K](v: Enc, k: PaillierEnc => K) extends CryptoF[K]
 case class ToGamal[K](v: Enc, k: GamalEnc => K) extends CryptoF[K]
 case class ToAes[K](v: Enc, k: AesEnc => K) extends CryptoF[K]
 
+// TODO offline or encode into encryption?
+case class Sub[K](lhs: Enc, rhs: Enc, k: Enc => K) extends CryptoF[K]
+
 object CryptoF {
   object DSL {
     type CryptoM[A] = Free[CryptoF, A]
@@ -21,12 +24,14 @@ object CryptoF {
 
     def multiply(lhs: Enc, rhs: Enc): Crypto[Enc] = FreeAp.lift(Mult(lhs,rhs,identity))
     def add(lhs: Enc, rhs: Enc): Crypto[Enc] = FreeAp.lift(Plus(lhs,rhs,identity))
-    def equals(lhs: Enc, rhs: Enc): Crypto[Boolean] = FreeAp.lift(Equals(lhs,rhs,identity))
+    def equal(lhs: Enc, rhs: Enc): Crypto[Boolean] = FreeAp.lift(Equals(lhs,rhs,identity))
     def compare(lhs: Enc, rhs: Enc): Crypto[Ordering] = FreeAp.lift(Compare(lhs,rhs,identity))
     def encrypt(v: Int): Crypto[Enc] = FreeAp.lift(Encrypt(v,identity))
     def toPaillier(v: Enc): Crypto[PaillierEnc] = FreeAp.lift(ToPaillier(v,identity))
     def toGamal(v: Enc): Crypto[GamalEnc] = FreeAp.lift(ToGamal(v,identity))
     def toAes(v: Enc): Crypto[AesEnc] = FreeAp.lift(ToAes(v,identity))
+
+    def subtract(lhs: Enc, rhs: Enc): Crypto[Enc] = FreeAp.lift(Sub(lhs,rhs,identity))
   }
 
   // deriving Functor
@@ -40,6 +45,7 @@ object CryptoF {
       case ToPaillier(v,k) => ToPaillier(v,f compose k)
       case ToGamal(v,k) => ToGamal(v,f compose k)
       case ToAes(v,k) => ToAes(v,f compose k)
+      case Sub(lhs,rhs,k) => Sub(lhs,rhs,f compose k)
     }
   }
 }
