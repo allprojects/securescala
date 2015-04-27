268e4943c41bacc00d9ed628bd5f1136b4b30974
Markus Hauck <markus1189@gmail.com>
2015-04-27 11:05:24 +0200

    Add Aes to KeyRing

--- src/main/scala/crypto/KeyRing.scala
+++ src/main/scala/crypto/KeyRing.scala
@@ -4,15 +4,20 @@ import crypto.cipher._
 
 case class KeyRing(enc: EncKeys, dec: DecKeys)
 case class EncKeys(paillier: Paillier.PubKey, gamal: ElGamal.PubKey)
-case class DecKeys(paillier: Paillier.Decryptor, gamal: ElGamal.Decryptor)
+case class DecKeys(
+  paillier: Paillier.Decryptor,
+  gamal: ElGamal.Decryptor,
+  aesEnc: Aes.Encryptor,
+  aesDec: Aes.Decryptor)
 
 object KeyRing {
   def create: KeyRing = {
     val (_, paillierDec, paillierPub) = Paillier.create(1024)
     val (_, gamalDec, gamalPub) = ElGamal.create(1024)
+    val (aesEnc, aesDec) = Aes.create(Aes.B256)
 
     val encKeys = EncKeys(paillierPub, gamalPub)
-    val decKeys = DecKeys(paillierDec, gamalDec)
+    val decKeys = DecKeys(paillierDec, gamalDec, aesEnc, aesDec)
     KeyRing(encKeys,decKeys)
   }
 }
