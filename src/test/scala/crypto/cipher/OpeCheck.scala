781c99ed38fe2bb78724c417deba2846df1fd139
Markus Hauck <markus1189@gmail.com>
2015-05-03 11:08:59 +0200

    Order preserving property for ope

--- src/test/scala/crypto/cipher/OpeCheck.scala
+++ src/test/scala/crypto/cipher/OpeCheck.scala
@@ -6,14 +6,21 @@ import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Prop.BooleanOperators
 
-import crypto.TestUtils._
+import scalaz.syntax.order._
+import scalaz.std.math.bigInt._
 
 object OpeCheck extends Properties("OPE") {
-  val (encrypt,decrypt) = Ope.createNum(128)
+  val (encrypt,decrypt,key) = Ope.createNum(128)
 
-  property("decrypt · encrypt = id (Int)") =
-    forAll(arbitrary[Int].retryUntil(_>=0)) { (input: Int) =>
+  val allowedBigInts = arbitrary[BigInt] retryUntil (x => x >=0 && x.bitLength <= key.plainBits)
+
+  property("decrypt · encrypt = id (limited BigInt)") =
+    forAll(allowedBigInts) { (input: BigInt) =>
       decrypt(encrypt(input)) == input
     }
+
+  property("preserves ordering") = forAll(allowedBigInts,allowedBigInts) { (a: BigInt, b: BigInt) =>
+    a ?|? b == encrypt(a) ?|? encrypt(b)
+  }
 
 }
