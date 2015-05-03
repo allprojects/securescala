d72391a6043a72ce526f30dbaa0642444472426b
Markus Hauck <markus1189@gmail.com>
2015-05-03 10:31:00 +0200

    Move methods to dummy class
    in traits the following error occurs:
    ... has illegal modifiers: 0x501

--- src/main/scala/crypto/cipher/Ope.scala
+++ src/main/scala/crypto/cipher/Ope.scala
@@ -2,9 +2,21 @@ package crypto.cipher
 
 import java.security.SecureRandom
 
-trait Ope { // trait required for javah/native interface
+class OpeNative { // trait required for javah/native interface
+  @native
+  def nativeEncrypt(password: String, plaintext: String,
+    plainTextBits: Int, cipherTextBits: Int): String
+
+  @native
+  def nativeDecrypt(password: String, ciphertext: String, plainTextBits: Int, cipherTextBits: Int): String
+
+  }
+
+object Ope {
   private val numPlainTextBits = 64
   private val numCipherTextBits = 96
+
+  private val instance = new OpeNative
 
   case class Encryptor(f: BigInt => String) extends Function1[BigInt,String]{
     def apply(x: BigInt) = f(x)
@@ -16,7 +28,7 @@ trait Ope { // trait required for javah/native interface
 
   case class PrivKey(key: String, bits: Int, plainBits: Int, cipherBits: Int)
 
-  def createNum(bits: Int): (Encryptor,Decryptor) = {
+  def createNum(bits: Int): (Encryptor, Decryptor) = {
     val key = generateKey(bits, numPlainTextBits, numCipherTextBits)
     (Encryptor(encrypt(key)), Decryptor(decrypt(key)))
   }
@@ -24,27 +36,20 @@ trait Ope { // trait required for javah/native interface
   private def generateKey(bits: Int, plainBits: Int, cipherBits: Int): PrivKey =
     PrivKey(BigInt(bits, new SecureRandom).toString(32), bits, plainBits, cipherBits)
 
-  @native
-  def native_encrypt(password: String, plaintext: String,
-    plainTextBits: Int, cipherTextBits: Int): String
+  def encrypt(priv: PrivKey)(input: BigInt): String =
+    instance.nativeEncrypt(priv.key, input.toString, priv.plainBits, priv.cipherBits)
 
-  @native
-  def native_decrypt(password: String, ciphertext: String, plainTextBits: Int, cipherTextBits: Int): String
+  def decrypt(priv: PrivKey)(input: String): BigInt =
+    BigInt(instance.nativeDecrypt(priv.key, input, priv.plainBits, priv.cipherBits))
 
-  private def encrypt(priv: PrivKey)(input: BigInt): String =
-    native_encrypt(priv.key, input.toString, priv.plainBits, priv.cipherBits)
 
-  private def decrypt(priv: PrivKey)(input: String): BigInt =
-    BigInt(native_decrypt(priv.key, input, priv.plainBits, priv.cipherBits))
 }
 
-object Ope extends Ope
-
 object OpeTest extends App {
-  val (enc,dec) = Ope.createNum(128)
+  // val (enc,dec) = Ope.createNum(128)
 
-  val plain = BigInt("19980902")
-  println(plain)
-  println(enc(plain))
-  println(dec(enc(plain)))
+  // val plain = BigInt("19980902")
+  // println(plain)
+  // println(enc(plain))
+  // println(dec(enc(plain)))
 }
