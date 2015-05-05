4d43585b658bf07c9b1729ac42bd97e7bcea600e
Markus Hauck <markus1189@gmail.com>
2015-05-05 10:47:34 +0200

    New generators for ope specific values

--- src/test/scala/crypto/TestUtils.scala
+++ src/test/scala/crypto/TestUtils.scala
@@ -6,11 +6,14 @@ import org.scalacheck.Gen
 import crypto.cipher._
 
 object TestUtils {
-  val positiveInt = arbitrary[BigInt] retryUntil (_.signum == 1)
+  val posInt = arbitrary[BigInt] retryUntil (_.signum == 1)
 
-  def encryptedNumber(keyRing: KeyRing): Gen[Enc] = for {
+  def opeAllowedInts(key: Ope.PrivKey) =
+    posInt retryUntil (_.bitLength <= key.plainBits)
+
+  def encryptedNumber(keyRing: KeyRing)(g: Gen[BigInt]): Gen[Enc] = for {
     scheme <- Gen.oneOf("Paillier","ElGamal","AES","OPE")
-    i <- positiveInt
+    i <- g
   } yield 
     scheme match {
       case "Paillier" => Common.encrypt(Additive, keyRing.pub)(i)
@@ -22,11 +25,11 @@ object TestUtils {
 
   def encryptedList(maxSize: Int)(keys: KeyRing): Gen[List[Enc]] = for {
     n <- Gen.choose(0,maxSize)
-    xs <- Gen.listOfN(n, encryptedNumber(keys))
+    xs <- Gen.listOfN(n, encryptedNumber(keys)(posInt))
   } yield xs
 
   def nonEmptyEncryptedList(maxSize: Int)(keys: KeyRing): Gen[List[Enc]] = for {
-    n <- encryptedNumber(keys)
+    n <- encryptedNumber(keys)(posInt)
     ns <- encryptedList(maxSize-1)(keys)
   } yield n :: ns
 }
