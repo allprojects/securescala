6bacc2f808e3c7e3aa452207489b5078a77c4e68
Markus Hauck <markus1189@gmail.com>
2015-05-08 09:33:49 +0200

    Implement CryptoService API

--- src/main/scala/crypto/remote/CryptoService.scala
+++ src/main/scala/crypto/remote/CryptoService.scala
@@ -7,26 +7,48 @@ import scala.concurrent._
 import scala.concurrent.duration._
 
 import crypto._
+import crypto.cipher._
 
 trait CryptoService {
-  def toPaillier: Enc => Future[PaillierEnc]
+  def toPaillier(in: Enc): Future[PaillierEnc]
+  def toElGamal(in: Enc): Future[GamalEnc]
+  def toAes(in: Enc): Future[AesEnc]
+  def toOpe(in: Enc): Future[OpeEnc]
 }
 
 class CryptoServiceImpl(keyRing: KeyRing) extends CryptoService {
-  val KeyRing(pub,priv) = keyRing
+  val convert: (Scheme, Enc) => Enc = Common.convert(keyRing)
 
-  override def toPaillier: Enc => Future[PaillierEnc] = _ match {
-    case x@PaillierEnc(_) => Future.successful(x)
-    case x => sys.error(s"Don't know how to convert to paillier: ${x}")
+  override def toPaillier(in: Enc): Future[PaillierEnc] = Future.successful {
+    val r@PaillierEnc(_) = convert(Additive, in)
+    r
+  }
+
+  override def toElGamal(in: Enc): Future[GamalEnc] = Future.successful {
+    val r@GamalEnc(_,_) = convert(Multiplicative, in)
+    r
+  }
+
+  override def toAes(in: Enc): Future[AesEnc] = Future.successful {
+    val r@AesEnc(_) = convert(Equality, in)
+    r
+  }
+
+  override def toOpe(in: Enc): Future[OpeEnc] = Future.successful {
+    val r@OpeEnc(_) = convert(Comparable, in)
+    r
   }
 }
 
 object CryptoServiceActor extends App {
 
+  val keyRing = KeyRing.create
+
   val system = ActorSystem("CryptoService")
 
   val cryptoService: CryptoService =
-    TypedActor(system).typedActorOf(TypedProps[CryptoServiceImpl]())
+    TypedActor(system).typedActorOf(TypedProps(classOf[CryptoService],
+      new CryptoServiceImpl(keyRing)), "cryptoServer")
 
   val response: Future[Enc] = cryptoService.toPaillier(PaillierEnc(1))
   println { Await.result(response, 1.second) }
