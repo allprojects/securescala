344510310f12a40e91a3c556291bb0c4d8e758ae
Markus Hauck <markus1189@gmail.com>
2015-05-08 10:08:49 +0200

    Start implementation of remote interpreter

---
+++ src/main/scala/crypto/dsl/RemoteInterpreter.scala
@@ -0,0 +1,79 @@
+package crypto.dsl
+
+import scalaz._
+import scalaz.std.scalaFuture._
+import scalaz.syntax.bind._
+
+import scala.concurrent._
+
+import crypto._
+import crypto.cipher._
+import crypto.remote._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+case class RemoteInterpreter(service: CryptoService) extends CryptoInterpreter[Future] {
+  def interpret[A] = _.resume match {
+    // Multiplication
+    case -\/(Mult(lhs@GamalEnc(_,_),rhs@GamalEnc(_,_),k)) => interpret(k(lhs * rhs))
+    case -\/(Mult(lhs,rhs,k)) => for {
+      lhs_ <- service.toElGamal(lhs)
+      rhs_ <- service.toElGamal(rhs)
+      r <- interpret(k(lhs_ * rhs_))
+    } yield r
+
+    // Addition
+    case -\/(Plus(lhs@PaillierEnc(_),rhs@PaillierEnc(_),k)) => interpret(k(lhs+rhs))
+    case -\/(Plus(lhs,rhs,k)) => for {
+      lhs_ <- service.toPaillier(lhs)
+      rhs_ <- service.toPaillier(rhs)
+      r <- interpret(k(lhs_ + rhs_))
+    } yield r
+
+    // Comparisons
+    case -\/(Compare(lhs,rhs,k)) => sys.error("No scheme for comparison")
+
+    // Equality
+    case -\/(Equals(lhs@AesEnc(_),rhs@AesEnc(_),k)) => interpret(k(lhs =:= rhs))
+    case -\/(Equals(lhs,rhs,k)) => for {
+      lhs_ <- service.toAes(lhs)
+      rhs_ <- service.toAes(rhs)
+      r <- interpret(k(lhs_ =:= rhs_))
+    } yield r
+
+    case -\/(ToPaillier(v,k)) => v match {
+      case v2@PaillierEnc(_) => interpret(k(v2))
+      case _ => service.toPaillier(v).flatMap(x => interpret(k(x)))
+    }
+
+    case -\/(ToGamal(v,k)) => v match {
+      case v2@GamalEnc(_,_) => interpret(k(v2))
+      case _ => service.toElGamal(v).flatMap(x => interpret(k(x)))
+    }
+
+    case -\/(ToAes(v,k)) => v match {
+      case v2@AesEnc(_) => interpret(k(v2))
+      case _ => service.toAes(v).flatMap(x => interpret(k(x)))
+    }
+
+    case -\/(ToOpe(v,k)) => v match {
+      case v2@OpeEnc(_) => interpret(k(v2))
+      case _ => service.toOpe(v).flatMap(x => interpret(k(x)))
+    }
+
+    // Encryption
+    case -\/(Encrypt(v,k)) => sys.error("encryption")
+
+      // Offline operations?
+    case -\/(Sub(lhs,rhs,k)) => sys.error("subtraction")
+
+    case -\/(Div(lhs,rhs,k)) => sys.error("division")
+
+    case -\/(Embed(p,k)) =>
+      val r: CryptoM[A] = k(Free.point(interpretA(p))).join
+      interpret(r)
+
+    // End of the program, return the final value
+    case \/-(x) => Future.successful(x)
+  }
+}
