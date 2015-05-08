c907d6eda2b0dfb8a5ed37db333f1c8ac271587c
Markus Hauck <markus1189@gmail.com>
2015-05-08 11:32:13 +0200

    Expand some analysis methods

--- src/main/scala/crypto/dsl/Analyze.scala
+++ src/main/scala/crypto/dsl/Analyze.scala
@@ -4,14 +4,20 @@ import scalaz._
 import scalaz.std.anyVal._
 
 import crypto._
+import crypto.cipher._
 
 object Analysis {
   def requiredConversions[A](p: Crypto[A]): Int = {
     p.analyze(new (CryptoF ~> λ[α => Int]) {
       def apply[B](fa: CryptoF[B]): Int = fa match {
         case ToPaillier(PaillierEnc(_),_) => 0
+        case ToPaillier(_,_) => 1
         case ToGamal(GamalEnc(_,_),_) => 0
+        case ToGamal(_,_) => 1
         case ToAes(AesEnc(_),_) => 0
+        case ToAes(_,_) => 1
+        case ToOpe(OpeEnc(_),_) => 0
+        case ToOpe(_,_) => 1
 
         case Plus(PaillierEnc(_),PaillierEnc(_),_) => 0
         case Plus(_,PaillierEnc(_),_) => 1
@@ -23,12 +29,12 @@ object Analysis {
         case Mult(GamalEnc(_,_),_,_) => 1
         case Mult(_,_,_) => 2
 
-        // TODO cases for the other
-        case x => 0
+        case x => sys.error(s"don't know how many conversion for ${x}")
       }
     })
   }
 
+  // eliminate all trivial conversions, e.g. toPaillier(PaillierEnc(_))
   def optimize[A](p: Crypto[A]): Crypto[A] = {
     p.foldMap(new (CryptoF ~> Crypto) {
       def apply[A](fa: CryptoF[A]): Crypto[A] = {
