a038db09d2783cd41582dd5617189fa45cb96038
Markus Hauck <markus1189@gmail.com>
2015-05-13 10:01:05 +0200

    Modify interpreter benchmarks for serializable problems
    - mark fields as transient for remote interpreter and scala check gens

--- src/test/scala/crypto/dsl/InterpreterBench.scala
+++ src/test/scala/crypto/dsl/InterpreterBench.scala
@@ -4,54 +4,55 @@ import scala.language.higherKinds
 
 import scala.concurrent._
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
 
 import scalaz.std.list._
 
 import org.scalacheck.{Gen => SCGen}
 
+import org.scalameter.api._
+
 import crypto._
 import crypto.cipher._
 import crypto.remote._
 
-import org.scalameter.api._
-
-trait InterpreterBench[F[_]] extends CryptoCheck {
+trait InterpreterBench[F[_]] {
   this: PerformanceTest =>
+
+  val keyRing = KeyRing.create
+  @transient val generators = EncryptedGens(keyRing)
 
   val zero = Common.zero(keyRing)
   val one = Common.one(keyRing)
 
-  val sizes = Gen.range("size")(10,50,20)
-  val lists = for (size <- sizes) yield SCGen.listOfN(size, generators.encryptedNumber).sample.get
+  val sizes = Gen.enumeration("size")(2,4,6,8,10)
+  val lists =
+    for (size <- sizes) yield SCGen.listOfN(size, generators.encryptedNumber).sample.get
 
   // To be implemented
   def finalize[A]: F[A] => A
   def interpret[A]: CryptoM[A] => F[A]
+  def name: String
 
   // The specific test cases
-  performance of "Sum" in {
-    measure method "monadic" in {
+  performance of name in {
+    measure method "monadic sum" in {
       using(lists) in { xs => finalize { interpret(sumM(zero)(xs)) } }
     }
 
-    measure method "applicative" in {
+    measure method "applicative sum" in {
       using(lists) in { xs => finalize { interpret(sumA(zero)(xs)) } }
     }
-  }
 
-  performance of "Product" in {
-    measure method "monadic" in {
+    measure method "monadic product" in {
       using(lists) in { xs => finalize { interpret(productM(one)(xs)) } }
     }
 
-    measure method "applicative" in {
+    measure method "applicative product" in {
       using(lists) in { xs => finalize { interpret(productA(one)(xs)) } }
     }
-  }
 
-  performance of "Sort" in {
-    measure method "applicative" in {
+    measure method "sorting" in {
       using(lists) in { xs => finalize { interpret(sorted(xs)) } }
     }
   }
@@ -61,8 +62,11 @@ object RemoteInterpreterBench
     extends PerformanceTest.OfflineReport
     with InterpreterBench[Future] {
 
-  val cryptoService = new CryptoServiceImpl(keyRing)
-  val interpreter = new RemoteInterpreter(cryptoService)
+  def name = "Remote interpreter (locally)"
+  @transient val cryptoService = new CryptoServiceImpl(keyRing)
+
+  @transient val interpreter =
+    new RemoteInterpreter(cryptoService)(ExecutionContext.Implicits.global)
 
   override def interpret[A] = (x: CryptoM[A]) => interpreter.interpret(x)
   override def finalize[A] = (x: Future[A]) => Await.result(x,Duration.Inf)
@@ -72,6 +76,7 @@ object LocalInterpreterBench
     extends PerformanceTest.OfflineReport
     with InterpreterBench[λ[α=>α]] {
 
+  def name = "Local interpreter"
   val interpreter = LocalInterpreter(keyRing)
 
   override def interpret[A] = (x: CryptoM[A]) => interpreter.interpret(x)
