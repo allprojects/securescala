191575b88a0de9864d379e97610a53f04d242b04
Markus Hauck <markus1189@gmail.com>
2015-05-13 12:02:26 +0200

    Add helper methods for conversions

--- src/main/scala/crypto/dsl/LocalInterpreter.scala
+++ src/main/scala/crypto/dsl/LocalInterpreter.scala
@@ -23,41 +23,46 @@ case class LocalInterpreter(keyRing: KeyRing) extends CryptoInterpreter[λ[α=>�
     })
   }
 
+  def additive(x: Enc): PaillierEnc = Common.depConvert(keyRing)(Additive, x)
+  def multiplicative(x: Enc): GamalEnc = Common.depConvert(keyRing)(Multiplicative, x)
+  def equality(x: Enc): AesEnc = Common.depConvert(keyRing)(Equality, x)
+  def comparable(x: Enc): OpeEnc = Common.depConvert(keyRing)(Comparable, x)
+
   def interpret[A]: CryptoM[A] => A = _.resume match {
 
     case -\/(Mult(lhs@GamalEnc(_,_),rhs@GamalEnc(_,_),k)) => interpret(k(lhs * rhs))
     case -\/(Mult(lhs,rhs,k)) =>
-      val lhs2 = Common.depConvert(keyRing)(Multiplicative, lhs)
-      val rhs2 = Common.depConvert(keyRing)(Multiplicative, rhs)
+      val lhs2 = multiplicative(lhs)
+      val rhs2 = multiplicative(rhs)
       interpret(k(lhs2*rhs2))
 
     case -\/(Plus(lhs@PaillierEnc(_),rhs@PaillierEnc(_),k)) => interpret(k(lhs+rhs))
     case -\/(Plus(lhs,rhs,k)) =>
-      val lhs2 = Common.depConvert(keyRing)(Additive, lhs)
-      val rhs2 = Common.depConvert(keyRing)(Additive, rhs)
+      val lhs2 = additive(lhs)
+      val rhs2 = additive(rhs)
       interpret(k(lhs2 + rhs2))
 
     case -\/(Compare(lhs@OpeEnc(_),rhs@OpeEnc(_),k)) => interpret(k(lhs ?|? rhs))
     case -\/(Compare(lhs,rhs,k)) =>
-      val lhs2 = Common.depConvert(keyRing)(Comparable, lhs)
-      val rhs2 = Common.depConvert(keyRing)(Comparable, rhs)
+      val lhs2 = comparable(lhs)
+      val rhs2 = comparable(rhs)
       interpret(k(lhs2 ?|? rhs2))
 
     case -\/(Equals(lhs@AesEnc(_),rhs@AesEnc(_),k)) => interpret(k(lhs =:= rhs))
     case -\/(Equals(lhs,rhs,k)) =>
-      val lhs2 = Common.depConvert(keyRing)(Equality,lhs)
-      val rhs2 = Common.depConvert(keyRing)(Equality,rhs)
+      val lhs2 = equality(lhs)
+      val rhs2 = equality(rhs)
       interpret(k(lhs2 =:= rhs2))
 
     case -\/(Encrypt(s,v,k)) => interpret(k(Common.encrypt(s, keyRing)(v)))
 
-    case -\/(ToPaillier(v,k)) => interpret(k(Common.depConvert(keyRing)(Additive,v)))
+    case -\/(ToPaillier(v,k)) => interpret(k(additive(v)))
 
-    case -\/(ToGamal(v,k)) => interpret(k(Common.depConvert(keyRing)(Multiplicative,v)))
+    case -\/(ToGamal(v,k)) => interpret(k(multiplicative(v)))
 
-    case -\/(ToAes(v,k)) => interpret(k(Common.depConvert(keyRing)(Equality,v)))
+    case -\/(ToAes(v,k)) => interpret(k(equality(v)))
 
-    case -\/(ToOpe(v,k)) => interpret(k(Common.depConvert(keyRing)(Comparable,v)))
+    case -\/(ToOpe(v,k)) => interpret(k(comparable(v)))
 
     // Offline operations
 
