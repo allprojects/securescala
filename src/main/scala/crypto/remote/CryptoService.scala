9ab4fab62d39581c5255a2b750c488138ac426ed
Markus Hauck <markus1189@gmail.com>
2015-05-26 12:52:00 +0200

    Extract wrapping of CryptoService for extensibility

--- src/main/scala/crypto/remote/CryptoService.scala
+++ src/main/scala/crypto/remote/CryptoService.scala
@@ -63,46 +63,48 @@ class CryptoServiceImpl(keyRing: KeyRing) extends CryptoService with CryptoServi
   private def equality(x: Enc): AesEnc = doConvert(Equality, x)
   private def comparable(x: Enc): OpeEnc = doConvert(Comparable, x)
 
-  override def publicKeys = Future.successful(keyRing.pub)
+  def wrap[A](x: A): Future[A] = Future.successful(x)
 
-  override def toPaillier(in: Enc): Future[PaillierEnc] = Future.successful(additive(in))
+  override def publicKeys = wrap(keyRing.pub)
+
+  override def toPaillier(in: Enc): Future[PaillierEnc] = wrap(additive(in))
   override def toElGamal(in: Enc): Future[ElGamalEnc] =
-    Future.successful(multiplicative(in))
-  override def toAes(in: Enc): Future[AesEnc] = Future.successful(equality(in))
-  override def toOpe(in: Enc): Future[OpeEnc] = Future.successful(comparable(in))
+    wrap(multiplicative(in))
+  override def toAes(in: Enc): Future[AesEnc] = wrap(equality(in))
+  override def toOpe(in: Enc): Future[OpeEnc] = wrap(comparable(in))
 
   override def convert(s: Scheme)(in: Enc): Future[Enc] =
-    Future.successful(Common.convert(keyRing)(s,in))
+    wrap(Common.convert(keyRing)(s,in))
   override def batchConvert(xs: List[(Scheme,Enc)]): Future[List[Enc]] =
-    Future.successful(xs.par.map { case (s,e) => Common.convert(keyRing)(s,e)}.toList)
+    wrap(xs.par.map { case (s,e) => Common.convert(keyRing)(s,e)}.toList)
 
   override def encrypt(s: Scheme)(in: Int): Future[Enc] =
-    Future.successful(Common.encrypt(s, keyRing)(in))
+    wrap(Common.encrypt(s, keyRing)(in))
   override def batchEncrypt(xs: List[(Scheme,Int)]): Future[List[Enc]] =
-    Future.successful(xs.par.map { case (s,i) => Common.encrypt(s,keyRing)(i)}.toList)
+    wrap(xs.par.map { case (s,i) => Common.encrypt(s,keyRing)(i)}.toList)
 
   override def decryptAndPrint(v: Enc): Unit = println(Common.decrypt(keyRing.priv)(v))
   override def println[A](a: A): Unit = Predef.println(a)
 
-  override def integerDivide(lhs: Enc, rhs: Enc): Future[Enc] = Future.successful {
+  override def integerDivide(lhs: Enc, rhs: Enc): Future[Enc] = wrap {
     val plainLhs = Common.decrypt(keyRing.priv)(lhs)
     val plainRhs = Common.decrypt(keyRing.priv)(rhs)
     val result = plainLhs / plainRhs
     Common.encrypt(Additive, keyRing)(result)
   }
 
-  override def subtract(lhs: Enc, rhs: Enc): Future[Enc] = Future.successful {
+  override def subtract(lhs: Enc, rhs: Enc): Future[Enc] = wrap {
     val plainLhs = Common.decrypt(keyRing.priv)(lhs)
     val plainRhs = Common.decrypt(keyRing.priv)(rhs)
     val result = plainLhs - plainRhs
     Common.encrypt(Additive, keyRing)(result)
   }
 
-  override def isEven(enc: Enc): Future[Boolean] = Future.successful {
+  override def isEven(enc: Enc): Future[Boolean] = wrap {
     Common.decrypt(keyRing.priv)(enc).mod(2) == 0
   }
 
-  override def isOdd(enc: Enc): Future[Boolean] = Future.successful {
+  override def isOdd(enc: Enc): Future[Boolean] = wrap {
     Common.decrypt(keyRing.priv)(enc).mod(2) == 1
   }
 }
