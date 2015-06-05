a1f85c73e115e12315154668358f3e30e833bee0
Markus Hauck <markus1189@gmail.com>
2015-06-05 10:56:49 +0200

    Allow negative numbers in ElGamal

--- src/test/scala/crypto/cipher/ElGamalCheck.scala
+++ src/test/scala/crypto/cipher/ElGamalCheck.scala
@@ -12,20 +12,21 @@ import crypto._
 object ElGamalCheck extends Properties("ElGamal") with CryptoCheck {
   val (encrypt,decrypt,pub) = ElGamal.create(1024)
 
-  property("decrypt · encrypt = id for positive ints") =
-    forAll(posInt) { (input: BigInt) =>
-      encrypt(input).map(decrypt.apply) == \/-(input)
+  property("decrypt · encrypt = id (Int)") =
+    forAll { (input: Int) =>
+      encrypt(input).map(decrypt(_)) == \/-(input)
     }
 
-  property("decrypt · encrypt = id (with modulus)") = forAll { (input: BigInt) =>
-    encrypt(input).map(decrypt.apply) == \/-(input.mod(pub.p))
+  property("decrypt · encrypt = id (limited BigInt)") =
+    forAll(generators.allowedNumber) { (input: BigInt) =>
+      encrypt(input).map(decrypt.apply) == \/-(input)
   }
 
-  property("multiplicative homomorphic") = forAll { (a: BigInt, b: BigInt) =>
+  property("multiplicative homomorphic") =
+    forAll(generators.allowedNumber, generators.allowedNumber) { (a: BigInt, b: BigInt) =>
     val \/-((ca1,ca2)) = encrypt(a)
     val \/-((cb1,cb2)) = encrypt(b)
 
-    decrypt(ca1 * cb1, ca2 * cb2).mod(pub.p) == (a * b).mod(pub.p)
+    decrypt(ca1 * cb1, ca2 * cb2) == (a * b)
   }
-
 }
