9b4e442bc83ace823f67bf3c5a15ea2e98d415aa
Markus Hauck <markus1189@gmail.com>
2015-06-10 12:00:44 +0200

    Add string ope to KeyRing

--- src/main/scala/crypto/KeyRing.scala
+++ src/main/scala/crypto/KeyRing.scala
@@ -11,7 +11,11 @@ case class PrivKeys(
   aesDec: Aes.Decryptor,
   opeIntEnc: OpeInt.Encryptor,
   opeIntDec: OpeInt.Decryptor,
-  opeIntPriv: OpeInt.PrivKey)
+  opeIntPriv: OpeInt.PrivKey,
+  opeStrEnc: OpeStr.Encryptor,
+  opeStrDec: OpeStr.Decryptor,
+  opeStrPriv: OpeStr.PrivKey
+)
 
 object KeyRing {
   implicit def narrowToPublicKeys: KeyRing => PubKeys = _.pub
@@ -20,9 +24,14 @@ object KeyRing {
     val (_, elgamalDec, elgamalPub) = ElGamal.create(1024)
     val (aesEnc, aesDec) = Aes.create(Aes.B256)
     val (opeIntEnc, opeIntDec,opeIntPriv) = OpeInt.create(128)
+    val (opeStrEnc, opeStrDec,opeStrPriv) = OpeStr.create(128, 10)
 
     val encKeys = PubKeys(paillierPub, elgamalPub)
-    val decKeys = PrivKeys(paillierDec, elgamalDec, aesEnc, aesDec, opeIntEnc, opeIntDec, opeIntPriv)
+    val decKeys = PrivKeys(
+      paillierDec, elgamalDec, aesEnc, aesDec,
+      opeIntEnc, opeIntDec, opeIntPriv,
+      opeStrEnc, opeStrDec, opeStrPriv
+    )
     KeyRing(encKeys,decKeys)
   }
 }
