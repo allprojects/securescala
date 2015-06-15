4b5c05e4bbab691a2c43eb829686a2439bafa6eb
Markus Hauck <markus1189@gmail.com>
2015-06-15 10:39:46 +0200

    Start spec for json printing/parsing

---
+++ src/test/scala/crypto/EncSpec.scala
@@ -0,0 +1,34 @@
+package crypto.dsl
+
+import argonaut._
+import Argonaut._
+
+import org.scalatest._
+
+import crypto._
+import crypto.cipher._
+
+class EncSpec extends WordSpec with Matchers {
+  val keys = KeyRing.create
+  implicit val paillierEncJson =
+    PaillierEnc.PaillierEncJson(keys.pub.paillier)
+
+  "Paillier encoded values" can {
+    "be printed as json" in {
+      val fortyTwo: PaillierEnc = Common.depEncrypt(Additive, keys)(42)
+      fortyTwo.asJson.nospaces should equal(s"""{"paillier":${fortyTwo.underlying}}""")
+    }
+
+    "be parsed from json" in {
+      val fortyTwo: PaillierEnc = Common.depEncrypt(Additive, keys)(42)
+      val json = s"""{"paillier":${fortyTwo.underlying}}"""
+      val dec = Parse.decodeOption[PaillierEnc](json)
+      dec should equal(Some(fortyTwo))
+    }
+  }
+
+  "ElGamal encoded values" can {
+    "be printed as json" in {
+    }
+  }
+}
