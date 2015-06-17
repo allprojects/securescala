e196d2d32e008425865095250ecb89fe23eaf367
Markus Hauck <markus1189@gmail.com>
2015-06-17 11:30:18 +0200

    Restructure nested pattern matches in interpreters

--- src/main/scala/crypto/dsl/LocalInterpreter.scala
+++ src/main/scala/crypto/dsl/LocalInterpreter.scala
@@ -19,77 +19,78 @@ case class LocalInterpreter(keyRing: KeyRing) extends PureCryptoInterpreter {
     Common.encryptStrOpe(keyRing)(Common.decryptStr(keyRing)(x))
 
   override def interpret[A](p: CryptoM[A]): A = p.resume match {
-    case -\/(Coproduct(-\/(Mult(lhs@ElGamalEnc(_,_),rhs@ElGamalEnc(_,_),k)))) =>
-      interpret(k(lhs * rhs))
-    case -\/(Coproduct(-\/(Mult(lhs,rhs,k)))) =>
-      interpret(k(multiplicative(lhs)*multiplicative(rhs)))
+    case -\/(next) => next match {
+      case Coproduct(-\/(cf)) => cf match {
+        case Mult(lhs@ElGamalEnc(_,_),rhs@ElGamalEnc(_,_),k) =>
+          interpret(k(lhs * rhs))
+        case Mult(lhs,rhs,k) =>
+          interpret(k(multiplicative(lhs)*multiplicative(rhs)))
 
-    case -\/(Coproduct(-\/(Plus(lhs@PaillierEnc(_),rhs@PaillierEnc(_),k)))) =>
-      interpret(k(lhs+rhs))
-    case -\/(Coproduct(-\/(Plus(lhs,rhs,k)))) =>
-      interpret(k(additive(lhs) + additive(rhs)))
+        case Plus(lhs@PaillierEnc(_),rhs@PaillierEnc(_),k) =>
+          interpret(k(lhs+rhs))
+        case Plus(lhs,rhs,k) =>
+          interpret(k(additive(lhs) + additive(rhs)))
 
-    case -\/(Coproduct(-\/(Compare(lhs@OpeEnc(_),rhs@OpeEnc(_),k)))) =>
-      interpret(k(lhs ?|? rhs))
-    case -\/(Coproduct(-\/(Compare(lhs,rhs,k)))) =>
-      interpret(k(comparable(lhs) ?|? comparable(rhs)))
+        case Compare(lhs@OpeEnc(_),rhs@OpeEnc(_),k) =>
+          interpret(k(lhs ?|? rhs))
+        case Compare(lhs,rhs,k) =>
+          interpret(k(comparable(lhs) ?|? comparable(rhs)))
 
-    case -\/(Coproduct(-\/(CompareStr(lhs@OpeString(_),rhs@OpeString(_),k)))) =>
-      interpret(k(lhs ?|? rhs))
-    case -\/(Coproduct(-\/(CompareStr(lhs,rhs,k)))) =>
-      interpret(k(comparableStr(lhs) ?|? comparableStr(rhs)))
+        case CompareStr(lhs@OpeString(_),rhs@OpeString(_),k) =>
+          interpret(k(lhs ?|? rhs))
+        case CompareStr(lhs,rhs,k) =>
+          interpret(k(comparableStr(lhs) ?|? comparableStr(rhs)))
 
-    case -\/(Coproduct(-\/(ConcatStr(lhs@OpeString(_),rhs@OpeString(_),k)))) =>
-      interpret(k(lhs |+| rhs))
-    case -\/(Coproduct(-\/(ConcatStr(lhs,rhs,k)))) =>
-      interpret(k(comparableStr(lhs) |+| comparableStr(rhs)))
+        case ConcatStr(lhs@OpeString(_),rhs@OpeString(_),k) =>
+          interpret(k(lhs |+| rhs))
+        case ConcatStr(lhs,rhs,k) =>
+          interpret(k(comparableStr(lhs) |+| comparableStr(rhs)))
 
-    case -\/(Coproduct(-\/(Equals(lhs@AesEnc(_),rhs@AesEnc(_),k)))) =>
-      interpret(k(lhs === rhs))
-    case -\/(Coproduct(-\/(Equals(lhs,rhs,k)))) =>
-      interpret(k(equality(lhs) === equality(rhs)))
+        case Equals(lhs@AesEnc(_),rhs@AesEnc(_),k) =>
+          interpret(k(lhs === rhs))
+        case Equals(lhs,rhs,k) =>
+          interpret(k(equality(lhs) === equality(rhs)))
 
-    case -\/(Coproduct(-\/(EqualsStr(lhs@AesString(_),rhs@AesString(_),k)))) =>
-      interpret(k(lhs === rhs))
-    case -\/(Coproduct(-\/(EqualsStr(lhs,rhs,k)))) =>
-      interpret(k(equalityStr(lhs) === equalityStr(rhs)))
+        case EqualsStr(lhs@AesString(_),rhs@AesString(_),k) =>
+          interpret(k(lhs === rhs))
+        case EqualsStr(lhs,rhs,k) =>
+          interpret(k(equalityStr(lhs) === equalityStr(rhs)))
 
-    case -\/(Coproduct(-\/(Encrypt(s,v,k)))) => interpret(k(Common.encrypt(s, keyRing)(v)))
+        case Encrypt(s,v,k) => interpret(k(Common.encrypt(s, keyRing)(v)))
 
-    case -\/(Coproduct(-\/(ToPaillier(v,k)))) => interpret(k(additive(v)))
-    case -\/(Coproduct(-\/(ToGamal(v,k)))) => interpret(k(multiplicative(v)))
-    case -\/(Coproduct(-\/(ToAes(v,k)))) => interpret(k(equality(v)))
-    case -\/(Coproduct(-\/(ToOpe(v,k)))) => interpret(k(comparable(v)))
-    case -\/(Coproduct(-\/(ToAesStr(v,k)))) => interpret(k(equalityStr(v)))
-    case -\/(Coproduct(-\/(ToOpeStr(v,k)))) => interpret(k(comparableStr(v)))
+        case ToPaillier(v,k) => interpret(k(additive(v)))
+        case ToGamal(v,k) => interpret(k(multiplicative(v)))
+        case ToAes(v,k) => interpret(k(equality(v)))
+        case ToOpe(v,k) => interpret(k(comparable(v)))
+        case ToAesStr(v,k) => interpret(k(equalityStr(v)))
+        case ToOpeStr(v,k) => interpret(k(comparableStr(v)))
 
-    // Offline operations
+          // Offline operations
 
-    case -\/(Coproduct(-\/(Sub(lhs,rhs,k)))) =>
-      val plainLhs = Common.decrypt(keyRing.priv)(lhs)
-      val plainRhs = Common.decrypt(keyRing.priv)(rhs)
-      val r = Common.encrypt(Additive, keyRing)(plainLhs - plainRhs)
-      interpret(k(r))
+        case Sub(lhs,rhs,k) =>
+          val plainLhs = Common.decrypt(keyRing.priv)(lhs)
+          val plainRhs = Common.decrypt(keyRing.priv)(rhs)
+          val r = Common.encrypt(Additive, keyRing)(plainLhs - plainRhs)
+          interpret(k(r))
 
-    case -\/(Coproduct(-\/(Div(lhs,rhs,k)))) =>
-      val plainLhs = Common.decrypt(keyRing.priv)(lhs)
-      val plainRhs = Common.decrypt(keyRing.priv)(rhs)
-      val r = Common.encrypt(Additive, keyRing)(plainLhs / plainRhs)
-      interpret(k(r))
+        case Div(lhs,rhs,k) =>
+          val plainLhs = Common.decrypt(keyRing.priv)(lhs)
+          val plainRhs = Common.decrypt(keyRing.priv)(rhs)
+          val r = Common.encrypt(Additive, keyRing)(plainLhs / plainRhs)
+          interpret(k(r))
 
-    case -\/(Coproduct(-\/(IsEven(v,k)))) =>
-      val plain = Common.decrypt(keyRing.priv)(v)
-      interpret(k(plain.mod(2) == 0))
+        case IsEven(v,k) =>
+          val plain = Common.decrypt(keyRing.priv)(v)
+          interpret(k(plain.mod(2) == 0))
 
-    case -\/(Coproduct(-\/(IsOdd(v,k)))) =>
-      val plain = Common.decrypt(keyRing.priv)(v)
-      interpret(k(plain.mod(2) == 1))
-
-    // Embedding
-    case -\/(Coproduct(\/-(e@Embed()))) =>
-      val r: CryptoM[A] = e.k(empower(Free.point(interpretA(e.v)))).join
-      interpret(r)
-
+        case IsOdd(v,k) =>
+          val plain = Common.decrypt(keyRing.priv)(v)
+          interpret(k(plain.mod(2) == 1))
+      }
+      case Coproduct(\/-(e@Embed())) =>
+        val r: CryptoM[A] = e.k(empower(Free.point(interpretA(e.v)))).join
+        interpret(r)
+    }
     case \/-(x) => x
   }
 }
--- src/main/scala/crypto/dsl/RemoteInterpreter.scala
+++ src/main/scala/crypto/dsl/RemoteInterpreter.scala
@@ -9,7 +9,7 @@ import crypto.remote._
 import scala.concurrent._
 import scala.io.StdIn
 import scala.util.Try
-import scalaz._
+  import scalaz._
 import scalaz.std.scalaFuture._
 import scalaz.syntax.bind._
 import scalaz.syntax.order._
@@ -19,118 +19,121 @@ case class RemoteInterpreter(service: CryptoServicePlus, pubKeys: PubKeys)(
   implicit ctxt: ExecutionContext) extends CryptoInterpreter[Future] {
 
   override def interpret[A](p: CryptoM[A]): Future[A] = p.resume match {
+    case -\/(next) => next match {
+      case Coproduct(-\/(cf)) => cf match {
+        case Mult(lhs@ElGamalEnc(_,_),rhs@ElGamalEnc(_,_),k) =>
+          interpret(k(lhs*rhs))
+        case Mult(lhs,rhs,k) => for {
+          lhs_ <- service.toElGamal(lhs)
+          rhs_ <- service.toElGamal(rhs)
+          r <- interpret(k(lhs_ * rhs_))
+        } yield r
 
-    case -\/(Coproduct(-\/(Mult(lhs@ElGamalEnc(_,_),rhs@ElGamalEnc(_,_),k)))) =>
-      interpret(k(lhs*rhs))
-    case -\/(Coproduct(-\/(Mult(lhs,rhs,k)))) => for {
-      lhs_ <- service.toElGamal(lhs)
-      rhs_ <- service.toElGamal(rhs)
-      r <- interpret(k(lhs_ * rhs_))
-    } yield r
+        case Plus(lhs@PaillierEnc(_),rhs@PaillierEnc(_),k) =>
+          interpret(k(lhs+rhs))
+        case Plus(lhs,rhs,k) => for {
+          lhs_ <- service.toPaillier(lhs)
+          rhs_ <- service.toPaillier(rhs)
+          r <- interpret(k(lhs_ + rhs_))
+        } yield r
 
-    case -\/(Coproduct(-\/(Plus(lhs@PaillierEnc(_),rhs@PaillierEnc(_),k)))) =>
-      interpret(k(lhs+rhs))
-    case -\/(Coproduct(-\/(Plus(lhs,rhs,k)))) => for {
-      lhs_ <- service.toPaillier(lhs)
-      rhs_ <- service.toPaillier(rhs)
-      r <- interpret(k(lhs_ + rhs_))
-    } yield r
+        case Compare(lhs@OpeEnc(_),rhs@OpeEnc(_),k) =>
+          interpret(k(lhs ?|? rhs))
+        case Compare(lhs,rhs,k) => for {
+          lhs_ <- service.toOpe(lhs)
+          rhs_ <- service.toOpe(rhs)
+          r <- interpret(k(lhs_ ?|? rhs_))
+        } yield r
 
-    case -\/(Coproduct(-\/(Compare(lhs@OpeEnc(_),rhs@OpeEnc(_),k)))) =>
-      interpret(k(lhs ?|? rhs))
-    case -\/(Coproduct(-\/(Compare(lhs,rhs,k)))) => for {
-      lhs_ <- service.toOpe(lhs)
-      rhs_ <- service.toOpe(rhs)
-      r <- interpret(k(lhs_ ?|? rhs_))
-    } yield r
+        case CompareStr(lhs@OpeString(_),rhs@OpeString(_),k) =>
+          interpret(k(lhs ?|? rhs))
+        case CompareStr(lhs,rhs,k) => for {
+          lhs_ <- service.toOpeStr(lhs)
+          rhs_ <- service.toOpeStr(rhs)
+          r <- interpret(k(lhs_ ?|? rhs_))
+        } yield r
 
-    case -\/(Coproduct(-\/(CompareStr(lhs@OpeString(_),rhs@OpeString(_),k)))) =>
-      interpret(k(lhs ?|? rhs))
-    case -\/(Coproduct(-\/(CompareStr(lhs,rhs,k)))) => for {
-      lhs_ <- service.toOpeStr(lhs)
-      rhs_ <- service.toOpeStr(rhs)
-      r <- interpret(k(lhs_ ?|? rhs_))
-    } yield r
+        case ConcatStr(lhs@OpeString(_),rhs@OpeString(_),k) =>
+          interpret(k(lhs |+| rhs))
+        case ConcatStr(lhs,rhs,k) => for {
+          lhs_ <- service.toOpeStr(lhs)
+          rhs_ <- service.toOpeStr(rhs)
+          r <- interpret(k(lhs_ |+| rhs_))
+        } yield r
 
-    case -\/(Coproduct(-\/(ConcatStr(lhs@OpeString(_),rhs@OpeString(_),k)))) =>
-      interpret(k(lhs |+| rhs))
-    case -\/(Coproduct(-\/(ConcatStr(lhs,rhs,k)))) => for {
-      lhs_ <- service.toOpeStr(lhs)
-      rhs_ <- service.toOpeStr(rhs)
-      r <- interpret(k(lhs_ |+| rhs_))
-    } yield r
+        case Equals(lhs@AesEnc(_),rhs@AesEnc(_),k) =>
+          interpret(k(lhs === rhs))
+        case Equals(lhs,rhs,k) => for {
+          lhs_ <- service.toAes(lhs)
+          rhs_ <- service.toAes(rhs)
+          r <- interpret(k(lhs_ === rhs_))
+        } yield r
 
-    case -\/(Coproduct(-\/(Equals(lhs@AesEnc(_),rhs@AesEnc(_),k)))) =>
-      interpret(k(lhs === rhs))
-    case -\/(Coproduct(-\/(Equals(lhs,rhs,k)))) => for {
-      lhs_ <- service.toAes(lhs)
-      rhs_ <- service.toAes(rhs)
-      r <- interpret(k(lhs_ === rhs_))
-    } yield r
+        case EqualsStr(lhs@AesString(_),rhs@AesString(_),k) =>
+          interpret(k(lhs === rhs))
+        case EqualsStr(lhs,rhs,k) => for {
+          lhs_ <- service.toAesStr(lhs)
+          rhs_ <- service.toAesStr(rhs)
+          r <- interpret(k(lhs_ === rhs_))
+        } yield r
 
-    case -\/(Coproduct(-\/(EqualsStr(lhs@AesString(_),rhs@AesString(_),k)))) =>
-      interpret(k(lhs === rhs))
-    case -\/(Coproduct(-\/(EqualsStr(lhs,rhs,k)))) => for {
-      lhs_ <- service.toAesStr(lhs)
-      rhs_ <- service.toAesStr(rhs)
-      r <- interpret(k(lhs_ === rhs_))
-    } yield r
+        case ToPaillier(v,k) => v match {
+          case v2@PaillierEnc(_) => interpret(k(v2))
+          case _ => service.toPaillier(v).flatMap(x => interpret(k(x)))
+        }
 
-    case -\/(Coproduct(-\/(ToPaillier(v,k)))) => v match {
-      case v2@PaillierEnc(_) => interpret(k(v2))
-      case _ => service.toPaillier(v).flatMap(x => interpret(k(x)))
-    }
+        case ToGamal(v,k) => v match {
+          case v2@ElGamalEnc(_,_) => interpret(k(v2))
+          case _ => service.toElGamal(v).flatMap(x => interpret(k(x)))
+        }
 
-    case -\/(Coproduct(-\/(ToGamal(v,k)))) => v match {
-      case v2@ElGamalEnc(_,_) => interpret(k(v2))
-      case _ => service.toElGamal(v).flatMap(x => interpret(k(x)))
-    }
+        case ToAes(v,k) => v match {
+          case v2@AesEnc(_) => interpret(k(v2))
+          case _ => service.toAes(v).flatMap(x => interpret(k(x)))
+        }
 
-    case -\/(Coproduct(-\/(ToAes(v,k)))) => v match {
-      case v2@AesEnc(_) => interpret(k(v2))
-      case _ => service.toAes(v).flatMap(x => interpret(k(x)))
-    }
+        case ToOpe(v,k) => v match {
+          case v2@OpeEnc(_) => interpret(k(v2))
+          case _ => service.toOpe(v).flatMap(x => interpret(k(x)))
+        }
 
-    case -\/(Coproduct(-\/(ToOpe(v,k)))) => v match {
-      case v2@OpeEnc(_) => interpret(k(v2))
-      case _ => service.toOpe(v).flatMap(x => interpret(k(x)))
-    }
+        case ToOpeStr(v,k) => v match {
+          case v2@OpeString(_) => interpret(k(v2))
+          case _ => service.toOpeStr(v).flatMap(x => interpret(k(x)))
+        }
 
-    case -\/(Coproduct(-\/(ToOpeStr(v,k)))) => v match {
-      case v2@OpeString(_) => interpret(k(v2))
-      case _ => service.toOpeStr(v).flatMap(x => interpret(k(x)))
-    }
+        case ToAesStr(v,k) => v match {
+          case v2@AesString(_) => interpret(k(v2))
+          case _ => service.toAesStr(v).flatMap(x => interpret(k(x)))
+        }
 
-    case -\/(Coproduct(-\/(ToAesStr(v,k)))) => v match {
-      case v2@AesString(_) => interpret(k(v2))
-      case _ => service.toAesStr(v).flatMap(x => interpret(k(x)))
-    }
+        case Encrypt(s,v,k) =>
+          val res: Future[EncInt] = s match {
+            // For public key encryption, we do not even have to send anything
+            case Additive =>
+              Future.successful(Common.depEncryptPub(Additive, pubKeys)(v))
+            case Multiplicative =>
+              Future.successful(Common.depEncryptPub(Multiplicative, pubKeys)(v))
+            case _ => service.encrypt(s)(v)
+          }
+          res.flatMap(x => interpret(k(x)))
 
-    case -\/(Coproduct(-\/(Encrypt(s,v,k)))) =>
-      val res: Future[EncInt] = s match {
-        // For public key encryption, we do not even have to send anything
-        case Additive =>
-          Future.successful(Common.depEncryptPub(Additive, pubKeys)(v))
-        case Multiplicative =>
-          Future.successful(Common.depEncryptPub(Multiplicative, pubKeys)(v))
-        case _ => service.encrypt(s)(v)
+        case Sub(lhs,rhs,k) =>
+          service.subtract(lhs,rhs).flatMap(x => interpret(k(x)))
+        case Div(lhs,rhs,k) =>
+          service.integerDivide(lhs,rhs).flatMap(x => interpret(k(x)))
+        case IsEven(v,k) =>
+          service.isEven(v).flatMap(x => interpret(k(x)))
+        case IsOdd(v,k) =>
+          service.isOdd(v).flatMap(x => interpret(k(x)))
       }
-      res.flatMap(x => interpret(k(x)))
 
-    case -\/(Coproduct(-\/(Sub(lhs,rhs,k)))) =>
-      service.subtract(lhs,rhs).flatMap(x => interpret(k(x)))
-    case -\/(Coproduct(-\/(Div(lhs,rhs,k)))) =>
-      service.integerDivide(lhs,rhs).flatMap(x => interpret(k(x)))
-    case -\/(Coproduct(-\/(IsEven(v,k)))) =>
-      service.isEven(v).flatMap(x => interpret(k(x)))
-    case -\/(Coproduct(-\/(IsOdd(v,k)))) =>
-      service.isOdd(v).flatMap(x => interpret(k(x)))
+      case Coproduct(\/-(em@Embed())) => for {
+        v <- interpretA(em.v)
+        r <- interpret(em.k(empower(Free.point(v))).join)
+      } yield r
 
-    case -\/(Coproduct(\/-(e@Embed()))) => for {
-      v <- interpretA(e.v)
-      r <- interpret(e.k(empower(Free.point(v))).join)
-    } yield r
-
+    }
     case \/-(x) => Future.successful(x)
   }
 }
