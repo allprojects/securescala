7b9761ba675856492cb58498b0ff3a800f736501
Markus Hauck <markus1189@gmail.com>
2015-06-23 18:39:37 +0200

    Extend build with example snippets

--- thesis/Build.hs
+++ thesis/Build.hs
@@ -1,5 +1,6 @@
 import Data.Char (isSpace)
-import Data.List (isPrefixOf)
+import Data.List (isPrefixOf, findIndex, isInfixOf)
+import Data.Maybe (fromMaybe)
 import Development.Shake
 import Development.Shake.FilePath
 
@@ -11,13 +12,33 @@ latexmkArgs = ["-r", ".latexmkrc"]
 
 main :: IO ()
 main = shakeArgs shakeOptions { shakeThreads = 4 } $ do
-  want ["thesis.pdf"]
-  "*.tex" %> \_ -> return ()
-  "*.pdf" %> \out -> (do
+   examples
+   texs
+   pdfs
+   want ["CryptoF.example","thesis.pdf"]
+
+texs :: Rules ()
+texs = "*.tex" %> const (return ())
+
+pdfs :: Rules ()
+pdfs = "*.pdf" %> \out -> (do
      let inp = out -<.> "tex"
      includes <- map extractInclude . filter isInclude <$> readFileLines inp
      need $ "bibliography.bib" : includes
      cmd latexmk latexmkArgs [inp])
+
+examples :: Rules ()
+examples = "*.example" %> \out -> (do
+    let inp = out -<.> "snippet"
+    [scalaFile,searchStart,searchEnd] <- readFileLines inp
+    need [inp,scalaFile]
+    fileContents <- readFileLines scalaFile
+    let start = fromMaybe (error "start not found") $
+          findIndex (searchStart `isInfixOf`) fileContents
+        end = fromMaybe (error "end not found") $
+          findIndex (searchEnd `isInfixOf`) fileContents
+    writeFileLines out (take (end - start + 1) (drop start fileContents))
+    )
 
 isInclude :: String -> Bool
 isInclude = ("\\include" `isPrefixOf`) . dropWhile isSpace
