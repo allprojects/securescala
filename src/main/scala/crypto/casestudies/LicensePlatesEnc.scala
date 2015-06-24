def1aa439eb655f16d22e770ccc692a8f974c1ea
Markus Hauck <markus1189@gmail.com>
2015-06-24 12:48:05 +0200

    Finish license plate case study query adaption

--- src/main/scala/crypto/casestudies/LicensePlatesEnc.scala
+++ src/main/scala/crypto/casestudies/LicensePlatesEnc.scala
@@ -13,13 +13,16 @@ package crypto.casestudies
 
 import com.espertech.esper.client._
 import com.espertech.esper.client.time._
+import crypto._
+import crypto.cipher._
+import crypto.dsl.Implicits._
+import crypto.dsl._
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Paths, Files}
 import scala.beans.BeanProperty
-import crypto._
-import crypto.cipher._
-import crypto.dsl._
-import crypto.dsl.Implicits._
+import scalaz.Ordering._
+import scalaz.std.list._
+import scalaz.syntax.traverse._
 
 sealed trait LicensePlateEventEnc {
   @BeanProperty def car: EncString
@@ -62,7 +65,6 @@ object LicensePlatesEnc extends App with EsperImplicits {
   val admin = epService.getEPAdministrator
 
   val speeders = admin.createEPL("""
-INSERT INTO Speeders
 SELECT car AS license, number, speed
 FROM CheckPointEventEnc
 WHERE Interp.isTooFast(speed)""")
@@ -75,20 +77,25 @@ WHERE Interp.isTooFast(speed)""")
   }
 
   val completions = admin.createEPL("""
-INSERT INTO CompleteCarRun
 SELECT s.time as startTime,
        g.time as goalTime,
        s.car as car,
-       g.time as goalTime,
-       s.time as startTime,
-       g.speed as maxSpeed
+       g.time - s.time as duration,
+       Interp.max(s.speed,c1.speed,c2.speed,c3.speed,g.speed) as maxSpeed
 FROM PATTERN [ every s=CarStartEventEnc
-               -> c1=CheckPointEventEnc(car=s.car,number=1)
-               -> c2=CheckPointEventEnc(car=c1.car,number=2)
-               -> c3=CheckPointEventEnc(car=c2.car,number=3)
-               -> g=CarGoalEventEnc(car=c3.car)
+               -> c1=CheckPointEventEnc(Interp.strEq(car,s.car),number=1)
+               -> c2=CheckPointEventEnc(Interp.strEq(car,c1.car),number=2)
+               -> c3=CheckPointEventEnc(Interp.strEq(car,c2.car),number=3)
+               -> g=CarGoalEventEnc(Interp.strEq(car,c3.car))
              ]
 """)
+
+  completions += { (es: Seq[EventBean]) =>
+    println(
+      f"${Interp.decryptStr(es.head.get("car").asInstanceOf[EncString])}%-9s " +
+      f"completed in ${es.head.get("duration").asInstanceOf[Long] / 1000}%ss " +
+      f"with speed ${Interp.decrypt(es.head.get("maxSpeed").asInstanceOf[EncInt])}%3s")
+  }
 
   def sendEvent(e: LicensePlateEventEnc): Unit = {
     if (rt.getCurrentTime != e.time) { // avoid duplicates
@@ -97,14 +104,16 @@ FROM PATTERN [ every s=CarStartEventEnc
     rt.sendEvent(e)
   }
 
-  val N = 1000
+  val N = 100
   val k = Interp.keyRing
   println(s"Generating events for ${N} different cars...")
   val evts = LicensePlateDataEnc.genEventsEnc(k)(N)
-  println("done!")
-  println(System.currentTimeMillis)
+  println(s"done! Generated ${evts.size} events")
+
+  val start = System.currentTimeMillis
   evts.foreach(sendEvent)
-  println(System.currentTimeMillis)
+  val end = System.currentTimeMillis
+  println(s"$Time for event processing: {(end - start) / 1000.0}s")
 }
 
 object Interp {
@@ -113,6 +122,12 @@ object Interp {
 
   val speedLimit = Common.encrypt(Comparable, keyRing)(133)
   def isTooFast(s: EncInt): Boolean = interpret(s > speedLimit)
+
+  def strEq(s1: EncString, s2: EncString) = interpret(s1 ?|? s2) == EQ
+  def max(i1: EncInt, i2: EncInt, i3: EncInt, i4: EncInt, i5: EncInt) =
+    interpret(List(i1,i2,i3,i4,i5).traverse(toOpe).map(_.max))
+
+
   val decryptStr = Common.decryptStr(keyRing)
   val decrypt = Common.decrypt(keyRing)
 }
@@ -131,6 +146,6 @@ object LicensePlateDataEnc {
   }
 
   def genEventsEnc(k: KeyRing)(n: Int) = {
-    LicensePlateData.genEvents(n).par.map(encryptEvent(k))
+    LicensePlateData.genEvents(n).map(encryptEvent(k))
   }
 }
