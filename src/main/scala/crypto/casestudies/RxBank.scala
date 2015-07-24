043e17ed00788bb73ca4bf41076ba5ab527ded4b
Markus Hauck <markus1189@gmail.com>
2015-07-24 12:42:44 +0200

    Basic RxBank processing observable transactions

--- src/main/scala/crypto/casestudies/RxBank.scala
+++ src/main/scala/crypto/casestudies/RxBank.scala
@@ -3,23 +3,78 @@
 // https://github.com/ReactiveX/RxJava/wiki/Additional-Reading
 package crypto.casestudies
 
-import scala.language.postfixOps
-import scala.concurrent.duration._
 import rx.lang.scala.Observable
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util._
+import scalaz.syntax.traverse._
+import scalaz.syntax.applicative._
+
+import crypto._
+import crypto.cipher._
+import crypto.dsl._
+import crypto.dsl.Implicits._
+
+import scalaz._
 
 object Hello {
-  def hello(names: String*): Unit = {
-    val _ = Observable.from(names) subscribe { n =>
-      println(s"Hello $n!")
+  val interp = LocalInterpreter.create
+  implicit val keyRing = interp.keyRing
+
+  def main(args: Array[String]) = {
+    val rand = new util.Random
+    val bank = new RxBank(interp, Common.zero(keyRing))
+    Observable.
+      interval(50 millis).
+      map(_ => Transaction.rand(rand)).
+      take(100).
+      toBlocking.
+      foreach(bank.process)
+    bank.print(keyRing)
+  }
+}
+
+class RxBank(interp: PureCryptoInterpreter, zero: EncInt) {
+  private var storage: OpeString ==>> EncInt = ==>>[OpeString,EncInt]()
+  def process: Transaction => Unit = { case Transaction(sender,receiver,amount) =>
+    val (senderOpe,receiverOpe) = interp {
+      toOpeStr(sender) tuple toOpeStr(receiver)
+    }
+
+    val (newSenderAmount,newReceiverAmount) = interp {
+      (storage.lookup(senderOpe).getOrElse(zero)-amount) tuple
+      (storage.lookup(receiverOpe).getOrElse(zero)+amount)
+    }
+
+    storage = storage.insert(senderOpe,newSenderAmount)
+    storage = storage.insert(receiverOpe,newReceiverAmount)
+  }
+
+  def print(implicit K: KeyRing): Unit = {
+    storage.toList.foreach { case (name,amount) =>
+      println(Common.decryptStr(K)(name) + ": " + Common.decrypt(K)(amount))
     }
   }
-  def main(args: Array[String]) = {
-    // Hello.hello("Markus")
-List(
-      Observable.interval(200 millis).map(_ => 1).take(5),
-      Observable.interval(200 millis).map(_ => 2).take(5),
-      Observable.interval(200 millis).map(_ => 3).take(5),
-      Observable.interval(200 millis).map(_ => 4).take(5)
-    ).toObservable.flatten(2).toBlocking.foreach(println(_))
+}
+
+case class Transaction(sender: EncString, receiver: EncString, amount: EncInt)
+
+object Transaction {
+  val senders = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toList
+  def rand(rand: Random)(implicit K: KeyRing): Transaction = {
+    val index = rand.nextInt(senders.length)
+    val sender = senders(index)
+
+    val remaining = senders.diff(List(sender))
+    val index2 = rand.nextInt(remaining.length)
+    val receiver = remaining(index2)
+
+    val amount = rand.nextInt(999)
+
+    Transaction(
+      Common.encryptStrAes(K)(sender.toString),
+      Common.encryptStrAes(K)(receiver.toString),
+      Common.encrypt(Additive,K)(amount)
+    )
   }
 }
