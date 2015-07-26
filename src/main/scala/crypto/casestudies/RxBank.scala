792841c21095fd7195b60429bad244bb1d4940d0
Markus Hauck <markus1189@gmail.com>
2015-07-26 09:59:30 +0200

    Flesh out RxBank case study

--- src/main/scala/crypto/casestudies/RxBank.scala
+++ src/main/scala/crypto/casestudies/RxBank.scala
@@ -20,32 +20,38 @@ import scalaz.syntax.traverse._
 
 import crypto._
 import crypto.cipher._
-import crypto.dsl._
 import crypto.dsl.Implicits._
+import crypto.dsl._
+import crypto.remote._
 
-object Hello {
+case class Transaction(sender: String, receiver: String, amount: EncInt)
+
+object Transaction {
   import RxBankConstants._
+  def rand(rand: Random)(implicit K: KeyRing): Transaction = {
+    val index = rand.nextInt(senders.length)
+    val sender = senders(index)
 
-  val interp = LocalInterpreter.create
-  implicit val keyRing = interp.keyRing
+    val remaining = senders.diff(List(sender))
+    val index2 = rand.nextInt(remaining.length)
+    val receiver = remaining(index2)
 
-  def main(args: Array[String]) = {
-    val rand = new Random
-    val bank = new RxBank(interp, Common.zero(keyRing))
-    Observable.
-      interval(50 millis).
-      map(_ => Transaction.rand(rand)).
-      take(100).
-      toBlocking.
-      foreach(bank.process)
-    bank.print(keyRing)
+    val amount = rand.nextInt(20) + 5
+
+    Transaction(
+      sender,
+      receiver,
+      Common.encrypt(Additive,K)(amount)
+    )
   }
 }
 
-class RxBank(interp: PureCryptoInterpreter, zero: EncInt) {
-  import RxBankConstants._
+class RxBank(ts: Observable[Transaction])(interp: PureCryptoInterpreter, zero: EncInt) {
   private var storage: Map[String,EncInt] = Map()
-  def process: Transaction => Unit = { case Transaction(sender,receiver,amount) =>
+
+  ts.foreach(process)
+
+  private def process: Transaction => Unit = { case Transaction(sender,receiver,amount) =>
     val (newSenderAmount,newReceiverAmount) = interp {
       (storage.get(sender).getOrElse(zero)-amount) tuple
       (storage.get(receiver).getOrElse(zero)+amount)
@@ -56,68 +62,10 @@ class RxBank(interp: PureCryptoInterpreter, zero: EncInt) {
   }
 
   def balance: String => EncInt = s => storage.get(s).getOrElse(zero)
-
-  def print(implicit K: KeyRing): Unit = {
-    storage.toList.foreach { case (name,amount) =>
-      println(name + ": " + Common.decrypt(K)(amount))
-    }
-  }
 }
 
-class RxBankInterfacePlain(model: RxBank, interp: PureCryptoInterpreter, keyRing: KeyRing)
-    extends SimpleSwingApplication {
-
-  import RxBankConstants._
-
-  val encColorMapping = colorMapping.map { case (thresh,c) => (interp(encrypt(Comparable)(thresh)),c)}
-
-  private def newField = new TextField {
-    text = ""
-    columns = 6
-    editable = false
-  }
-
-  private val fields = List.fill(senders.size)(newField)
-  val fieldFor = senders.zip(fields).toMap
-  val messages = new ListView[String](List()) {
-    preferredSize = new Dimension(300,500)
-  }
-
-  def top = new MainFrame {
-    title = "RxBank Plain"
-    contents = new BoxPanel(Orientation.Vertical) {
-      preferredSize = new Dimension(400,500)
-      senders.zipWithIndex.foreach { case (s,i) =>
-        contents += new FlowPanel(FlowPanel.Alignment.Leading)() {
-          contents += Swing.HStrut(60)
-          contents += fieldFor(s)
-          contents += new Label(s)
-        }
-      }
-      contents += new FlowPanel (new ScrollPane (messages))
-    }
-  }
-
-  def logTransaction(t: Transaction) = t match {
-    case Transaction(sender,receiver,amnt) =>
-      val old = messages.listData
-      messages.listData = s"${sender} -> ${receiver} (${Common.decrypt(keyRing)(amnt)})" +: old
-      redrawFor(sender)
-      redrawFor(receiver)
-  }
-
-  private def redrawFor(s: String): Unit = {
-    val field = fieldFor(s)
-    val amt = model.balance(s)
-    val colors = interp { encColorMapping.traverse { case (t,c) => (amt > t).map((_,c)) } }.filter(_._1)
-    field.text = Common.decrypt(keyRing)(amt).toString
-    if (colors.nonEmpty)
-      field.background = colors.last._2
-  }
-}
-
-class RxBankInterface(model: RxBank, interp: PureCryptoInterpreter)
-    extends SimpleSwingApplication {
+class RxBankInterface(ts: Observable[Transaction])(
+  model: RxBank, interp: PureCryptoInterpreter) extends SimpleSwingApplication {
 
   import RxBankConstants._
 
@@ -150,6 +98,8 @@ class RxBankInterface(model: RxBank, interp: PureCryptoInterpreter)
     }
   }
 
+  ts.foreach(logTransaction)
+
   def logTransaction(t: Transaction) = t match {
     case Transaction(sender,receiver,amnt) =>
       val old = messages.listData
@@ -165,36 +115,54 @@ class RxBankInterface(model: RxBank, interp: PureCryptoInterpreter)
     if (colors.nonEmpty)
       field.background = colors.last._2
   }
+}
 
+class RxBankInterfacePlain(ts: Observable[Transaction])(
+  model: RxBank, interp: PureCryptoInterpreter, keyRing: KeyRing)
+    extends RxBankInterface(ts)(model,interp) {
+
+  override def logTransaction(t: Transaction) = t match {
+    case Transaction(sender,receiver,amnt) =>
+      val old = messages.listData
+      messages.listData = s"${sender} -> ${receiver} (${Common.decrypt(keyRing)(amnt)})" +: old
+      redrawFor(sender)
+      redrawFor(receiver)
+  }
+
+  private def redrawFor(s: String): Unit = {
+    val field = fieldFor(s)
+    val amt = model.balance(s)
+    val colors = interp { encColorMapping.traverse { case (t,c) => (amt > t).map((_,c)) } }.filter(_._1)
+    field.text = Common.decrypt(keyRing)(amt).toString
+    if (colors.nonEmpty)
+      field.background = colors.last._2
+  }
 }
 
 object RxBankApp extends App {
-  import RxBankConstants._
-  val interp = LocalInterpreter.create
-  implicit val keyRing = interp.keyRing
+  implicit val keyRing = KeyRing.create
 
-  val bank = new RxBank(interp, Common.zero(keyRing))
-  val interface = new RxBankInterface(bank,interp)
-  val interfacePlain = new RxBankInterfacePlain(bank,interp,keyRing)
+  // val interp = new LocalInterpreter(keyRing)
+
+  val globalEC = scala.concurrent.ExecutionContext.Implicits.global
+  val cryptoService = new CryptoServiceImpl(keyRing)(globalEC)
+  val pubKeys = Await.result(cryptoService.publicKeys, 10.seconds)
+  val interp = Blocking {
+    new RemoteInterpreterOptAnalyze(cryptoService, pubKeys, FixedBatch(10), _ > 3)(globalEC)
+  }
 
   val rand = new Random
+  val ts = Observable.interval(200 millis).map(_ => Transaction.rand(rand))
 
-  Observable.
-    interval(200 millis).
-    map(_ => Transaction.rand(rand)).
-    foreach{ t =>
-      bank.process(t)
-      interface.logTransaction(t)
-      interfacePlain.logTransaction(t)
-    }
+  val bank = new RxBank(ts)(interp, Common.zero(keyRing))
 
-  val i1 = Future { interface.startup(Array()) }
-  val i2 = Future { interfacePlain.startup(Array()) }
-  val is = Future.sequence(List(i1,i2))
-  Await.result(is,Duration.Inf)
+  Await.result(Future.sequence(
+    List(
+      Future { new RxBankInterface(ts)(bank,interp).startup(Array()) }
+        , Future { new RxBankInterfacePlain(ts)(bank,interp,keyRing).startup(Array()) }
+    )
+  ),30.minutes)
 }
-
-
 
 object RxBankConstants {
   import java.awt.Color
@@ -218,27 +186,4 @@ object RxBankConstants {
     (50, new Color(87, 255, 69)),
     (100, new Color(0, 224, 14))
   )
-
-  case class Transaction(sender: String, receiver: String, amount: EncInt)
-
-  object Transaction {
-
-    def rand(rand: Random)(implicit K: KeyRing): Transaction = {
-      val index = rand.nextInt(senders.length)
-      val sender = senders(index)
-
-      val remaining = senders.diff(List(sender))
-      val index2 = rand.nextInt(remaining.length)
-      val receiver = remaining(index2)
-
-      val amount = rand.nextInt(20 + 5)
-
-      Transaction(
-        sender,
-        receiver,
-        Common.encrypt(Additive,K)(amount)
-      )
-    }
-  }
-
 }
