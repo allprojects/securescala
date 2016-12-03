b0419f4b7b6e4e259cc55c8ac8d666b750f460ce
Chen, Yiqun <feedingfranzy@gmail.com>
2016-12-03 16:51:41 +0100

    [PATCH]Add spark word count benchmark
    WordCountBench.scala:	Add benchmark test case for spark by
    			using scalameter and spark launcher
    WordCountSpark.scala:	Improve genericity by introducing new functions
    			Add program for benchmark

--- src/main/scala/crypto/casestudies/WordCountSpark.scala
+++ src/main/scala/crypto/casestudies/WordCountSpark.scala
@@ -12,9 +12,9 @@ object WordCountSpark {
   import scalax.file.Path
 
   val conf = new SparkConf().setAppName("Word Count on Spark")
-  val sc = new SparkContext(conf)
+  implicit val sc = new SparkContext(conf)
 
-  object WordCount {
+  object EncryptedFileOperations {
     def encryptContent(k: KeyRing)(input: String): EncString =
       Common.encryptStrOpe(k)(input)
 
@@ -24,20 +24,46 @@ object WordCountSpark {
 
     // RDD requires strict invariant
     // It handles all dirty works of fromString() parsing the file
-    def readEncryptedFile(fileName: String): RDD[OpeString] = 
+    def readEncryptedFile(fileName: String)(implicit sc: SparkContext): RDD[OpeString] = 
       sc.textFile(fileName).map(line => line.replaceAll("List|OpeString|[()]", "").split(", ")).map(x => x.map(BigInt(_)).toList).map(OpeString(_))
 
   }
 
+  def wordCount(content: RDD[String]): Array[(String, Int)] = content.flatMap(word => word.split("""\W+""")).map(x => (x, 1)).reduceByKey((l, r) => l + r).collect
+
   def main(args: Array[String]) = {
+    import EncryptedFileOperations._
 
     val keyring = KeyRing.create
 
     val plain = Source.fromFile(args(0)).mkString
-    WordCount.writeEncryptedFile(WordCount.encryptContent(keyring)(plain), "encrypted_test.txt")
+    writeEncryptedFile(encryptContent(keyring)(plain), "encrypted_test.txt")
 
-    val content = WordCount.readEncryptedFile("encrypted_test.txt")
-    content.map(Common.decryptStr(keyring.priv)).flatMap(word => word.split("""\W+""")).map(x => (x, 1)).reduceByKey((l, r) => l + r).collect.foreach(println)
+    val content = readEncryptedFile("encrypted_test.txt")
+    val result = wordCount(content.map(Common.decryptStr(keyring.priv)))
+    result.foreach(println)
+  }
+}
 
+object WordCountSparkBench {
+  import WordCountSpark._
+
+  import crypto._
+  import crypto.cipher._
+
+  import scala.io._
+  import scalax.file.Path
+
+  def main(args: Array[String]) = {
+    import EncryptedFileOperations._
+
+    val keyring = KeyRing.create
+
+    val plain = Source.fromFile(args(0)).mkString.take(args(1).toInt)
+    writeEncryptedFile(encryptContent(keyring)(plain), "encrypted_test.txt")
+
+    val content = readEncryptedFile("encrypted_test.txt")
+    wordCount(content.map(Common.decryptStr(keyring.priv)))
+    ()
   }
 }
